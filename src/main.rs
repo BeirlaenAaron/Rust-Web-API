de698198c367a34786f53269f2e0d959da8bdf2f
BeirlaenAaron <aaron.beirlaen@student.howest.be>
2022-01-12 16:15:34 +0100

    added docker with db setup

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 #[macro_use] extern crate rocket;
 
-#[get("/test")]
+#[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/yo", routes![index])
+    rocket::build().mount("/", routes![index])
 }
